8dd2030307a0d0af3ed30340e445eb8c5125bd39
Jermal Wynder <jermalwynder@gmail.com>
2024-05-25 01:01:57 -0400

    third solution set

--- R-ALAB-308-4-1.js
+++ R-ALAB-308-4-1.js
@@ -9,4 +9,51 @@ const csv2DArray = [
   [`98`, `Bill`, `Doctor's Assistant`, `26`],
 ];
 
+// 2D array demonstration for CSV
 console.log(csv2DArray);
+
+// Part 3: Transforming Data
+
+// Variable delcaration for array data to be transformed
+const array = [
+  [
+    [`id`, `42`],
+    [`name`, `Bruce`],
+    [`occupation`, `Knight`],
+    [`age`, `41`],
+  ],
+  [
+    [`id`, `57`],
+    [`name`, `Bob`],
+    [`occupation`, `Fry Cook`],
+    [`age`, `19`],
+  ],
+  [
+    [`id`, `63`],
+    [`name`, `Blaine`],
+    [`occupation`, `Quiz Master`],
+    [`age`, `58`],
+  ],
+  [
+    [`id`, `98`],
+    [`name`, "Bill"],
+    [`occupation`, `Doctor's Assistant`],
+    [`age`, `26`],
+  ],
+];
+
+// Method used to take in one such array,
+// and returns a new array of objects built based on the input array
+const arrayToObject = (array) => {
+  let result = [];
+  result = array.map((list) => {
+    return list.reduce((acc, val) => {
+      acc[val[0]] = val[1];
+      return acc;
+    }, {});
+  });
+  return result;
+};
+
+// Result of array transformation into objects transformation
+console.log(arrayToObject(array));
