76c94ddfbb07c1d45921ff0c9b721e15b4796c54
Jermal Wynder <jermalwynder@gmail.com>
2024-05-24 22:55:10 -0400

    first commit

---
+++ R-ALAB-308-4-1.js
@@ -0,0 +1,12 @@
+// Part 2: Expanding Functionality
+
+// Variable declaration for 2D array of CSV
+const csv2DArray = [
+  [`ID`, `Name`, `Occupation`, `Age`],
+  [`42`, `Bruce`, `Knight`, `41`],
+  [`57`, `Bob`, `Fry Cook`, `19`],
+  [`63`, `Blaine`, `Quiz Master`, `58`],
+  [`98`, `Bill`, `Doctor's Assistant`, `26`],
+];
+
+console.log(csv2DArray);
