119951c3e044521b04750c7565071ab64d072b23
Jermal Wynder <jermalwynder@gmail.com>
2024-05-25 09:23:46 -0400

    minor changes

--- R-ALAB-308-4-1.js
+++ R-ALAB-308-4-1.js
@@ -14,46 +14,31 @@ console.log(csv2DArray);
 
 // Part 3: Transforming Data
 
-// Variable delcaration for array data to be transformed
+// Variable delcaration for object data of CSV
 const array = [
-  [
-    [`id`, `42`],
-    [`name`, `Bruce`],
-    [`occupation`, `Knight`],
-    [`age`, `41`],
-  ],
-  [
-    [`id`, `57`],
-    [`name`, `Bob`],
-    [`occupation`, `Fry Cook`],
-    [`age`, `19`],
-  ],
-  [
-    [`id`, `63`],
-    [`name`, `Blaine`],
-    [`occupation`, `Quiz Master`],
-    [`age`, `58`],
-  ],
-  [
-    [`id`, `98`],
-    [`name`, "Bill"],
-    [`occupation`, `Doctor's Assistant`],
-    [`age`, `26`],
-  ],
+  {
+    id: `42`,
+    name: `Bruce`,
+    occupation: `Knight`,
+    age: `41`,
+  },
+  {
+    id: `57`,
+    name: `Bob`,
+    occupation: `Fry Cook`,
+    age: `19`,
+  },
+  {
+    id: `63`,
+    name: `Blaine`,
+    occupation: `Quiz Master`,
+    age: `58`,
+  },
+  {
+    id: `98`,
+    name: `Bill`,
+    occupation: `Doctor's Assistant`,
+    age: `26`,
+  },
 ];
-
-// Method used to take in one such array,
-// and returns a new array of objects built based on the input array
-const arrayToObject = (array) => {
-  let result = [];
-  result = array.map((list) => {
-    return list.reduce((acc, val) => {
-      acc[val[0]] = val[1];
-      return acc;
-    }, {});
-  });
-  return result;
-};
-
-// Result of array transformation into objects transformation
-console.log(arrayToObject(array));
+console.log(array);
